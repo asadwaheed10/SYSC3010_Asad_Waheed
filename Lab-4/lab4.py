08f02e0a691a0e8bd157fe05294b1c6247bffdf5
Asad Waheed <asadwaheed@cmail.carleton.ca>
2020-10-06 14:02:02 -0400

    deleted submodule and created a folder for lab4'

---
+++ Lab-4/lab4.py
@@ -0,0 +1,25 @@
+import http.client
+import urllib.parse
+import time
+key = "54CDA6FJF4RCJ92C"
+
+def thingspeak():
+	while True:
+		email = "asadwaheed@cmail.carleton.ca"
+		group = "L2-M-5"
+		identifier = "d"
+		params = urllib.parse.urlencode({'field1': email, 'field2': group, 'field3': identifier, 'key': key})
+		headers = {"Content-typeZZe": "application/x-www-form-urlencoded", "Accept": "text/plain"} 
+		conn = http.client.HTTPConnection("api.thingspeak.com:80")
+		try:
+			conn.request("POST", "/update", params, headers)
+			response = conn.getresponse()
+			print (response.status, response.reason)
+			data = response.read()
+			conn.close()
+		except:
+			print ("connection failed")
+		break
+if __name__ == "__main__":
+	while True:
+		thingspeak()
